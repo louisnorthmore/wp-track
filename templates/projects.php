2714a1a4a730f0d630f22af7e8bc0a1dd0fdb83b
louis <louis@barney.(none)>
2011-10-17 16:54:51 +0100

    improved template

--- templates/projects.php
+++ templates/projects.php
@@ -1,13 +1,42 @@
 <?php
-
-echo "projects.php";
-//WP-TRACK - PROJECTS TEMPLATE
-
-get_header();
-
-get_sidebar();
-
-get_footer();
-
-
+/*
+WP_TRACK - PROJECTS TEMPLATE
+*/
 ?>
+
+<?php get_header() ?>
+
+	<div id="content" class="fullwidth">
+		<div class="padder">
+
+		<?php do_action( 'bp_before_blog_page' ) ?>
+
+		<div class="page" id="blog-page">
+
+			<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+
+				<h1 class="pagetitle"><?php the_title(); ?></h1>
+
+				<div class="post" id="post-<?php the_ID(); ?>">
+
+					<div class="entry">
+
+						<?php the_content( __( '<p class="serif">Read the rest of this page &rarr;</p>', 'buddypress' ) ); ?>
+
+						<?php wp_link_pages( array( 'before' => __( '<p><strong>Pages:</strong> ', 'buddypress' ), 'after' => '</p>', 'next_or_number' => 'number')); ?>
+						<?php //edit_post_link( __( 'Edit this entry.', 'buddypress' ), '<p>', '</p>'); ?>
+
+					</div>
+
+				</div>
+
+			<?php endwhile; endif; ?>
+
+		</div><!-- .page -->
+
+		<?php do_action( 'bp_after_blog_page' ) ?>
+
+		</div><!-- .padder -->
+	</div><!-- #content -->
+
+<?php get_footer(); ?>
