a5c63277ea21efb8b209634b5525319828b95b6e
louis <louis@barney.(none)>
2011-10-19 14:25:04 +0100

    loads of additions

--- templates/projects.php
+++ templates/projects.php
@@ -5,28 +5,124 @@ WP_TRACK - PROJECTS TEMPLATE
 ?>
 
 <?php get_header() ?>
+<style>
+.updated {
+font-size: 14px;
+font-weight: bold;
+background-color:#FFFFE0;
+border: 1px #E6DB55 solid;
+padding: 15px;
+-moz-border-radius: 8px;
+border-radius: 8px;
+}
+</style>
+<?
+if( 'POST' == $_SERVER['REQUEST_METHOD'] && !empty( $_POST['action'] )) {
+
+	// Do some minor form validation to make sure there is content
+	if (isset ($_POST['name'])) {
+		$name =  $_POST['name'];
+	} else {
+		echo 'Please enter a name';
+	}
+	if (isset ($_POST['description'])) {
+		$description1 = $_POST['description'];
+	} else {
+		echo 'Please enter the content';
+	}
+    
+	wp_get_current_user();
+	$user = $current_user->user_login;
+	
+	$project_name = $_POST['project_name'];
+	// Add the content of the form to $post as an array
+	$post = array(
+		'post_title'	=> $name,
+		'post_content'	=> $description1,
+		'post_status'	=> 'publish',			// Choose: publish, preview, future, etc.
+		'comment_status' => 'open',
+		'post_type'	=> $_POST['post_type']
+	);
+	$postid = wp_insert_post($post); 
+	
+	wp_set_object_terms($postid, 'Medium', 'priority');
+	wp_set_object_terms($postid, 'Current', 'milestone');
+	wp_set_object_terms( $postid, 'Pending', 'status');
+	wp_set_object_terms( $postid, 'FCC Client', 'projects');
+	echo "<div class='updated'>Thanks $user! <br/>Bug ($postid) created and is status pending. It will be reviewed shortly.</div>";
+	mail('admin@funchatcam.com',"$user submitted a bug - $name","$user submitted a bug - $name");
+	
+} // end IF
+?>
+
+<script type='text/javascript'>
+function addbug() {
+document.getElementById('AddBug').style.display = 'block'; 
+document.getElementById('tasks').style.display = 'none'; 
+}
+</script>
 
 	<div id="content" class="fullwidth">
 		<div class="padder">
 
-		<?php do_action( 'bp_before_blog_page' ) ?>
-
 		<div class="page" id="blog-page">
 
+		<?php if (is_single()) { ?> 
+		
 			<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-				<h1 class="pagetitle">Project > <?php the_title(); ?></h1>
-
+				<h1 class="pagetitle"><a href='/projects/'>Projects</a> > <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+				
+				
+				
 				<div class="post" id="post-<?php the_ID(); ?>">
 
 					<div class="entry">
+					
+<p>
+Description: <?php echo get_the_content(); ?><br />
+Created: <?php the_date('jS F Y g:ia (e)'); ?><br />
+				Author: <?php the_author(); ?><br />
+				<a target="_blank" title="Edit Project" alt="Edit Project" href="/wp-admin/post.php?post=<?php echo get_the_ID(); ?>&action=edit">Edit Project</a>
+				</p>
+		<div id="tasks" name="tasks">
+		<?php list_project_tasks(get_the_title()); ?>
+		</div>
+		
+		<div id="Bugs" name="bugs">
+		
+		<div id="AddBug" style="display: none">
+		<h3>Add New Bug</h3>
+		<form id="new_post" name="new_post" method="post" action="">
 
-		<?php the_content( __( '<p class="serif">Read the rest of this page &rarr;</p>', 'buddypress' ) ); ?>
+<p><label for="name">Name</label><br />
 
-		<?php project_tasks(); ?>
+<input type="text" id="name" value="" tabindex="1" size="20" name="name" />
+
+</p>
+
+<p><label for="description">Description</label><br />
+
+<textarea id="description" tabindex="3" name="description" cols="50" rows="6"></textarea>
+
+</p>
+
+<p><input type="submit" value="Submit Bug" tabindex="6" id="submit" name="submit" /></p>
+
+<input type="hidden" name="post_type" id="post_type" value="track-bugs" />
+<input type="hidden" name="action" value="post" />
+<input type="hidden" name="project_name" value="<?php echo get_the_title(); ?>" />
+
+<?php wp_nonce_field( 'new-post' ); ?>
+
+</form>
+		</div>
+		
+		<?php list_project_bugs(get_the_title()); ?>
+		</div>
 
 <?php wp_link_pages( array( 'before' => __( '<p><strong>Pages:</strong> ', 'buddypress' ), 'after' => '</p>', 'next_or_number' => 'number')); ?>
-		<?php edit_post_link( __( 'Edit this project.', 'buddypress' ), '<p>', '</p>'); ?>
+		
 
 					</div>
 
@@ -34,11 +130,30 @@ WP_TRACK - PROJECTS TEMPLATE
 
 			<?php endwhile; endif; ?>
 
-		</div><!-- .page -->
 
-		<?php do_action( 'bp_after_blog_page' ) ?>
+<?php } else { ?> <!-- end single -->
+<h1 class="pagetitle">Projects</h1>
+<div class="post" id="post-<?php the_ID(); ?>">
+<div class="entry">
+<table class="wp-list-table widefat fixed posts" cellspacing="0">
+<th>Name</th><th>Author</th><th>Tasks</th><th>Bugs</th>
+<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+
+<tr>
+<td><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></td>
+<td><?php the_author(); ?></td>
+<td><?php count_posts('track-tasks', get_the_title()) ?></td>
+<td><?php count_posts('track-bugs', get_the_title()) ?></td>
+</tr>
+		</div>
+
+			<?php endwhile; endif; ?>
+</table>
+</div>
+</div>
+<?php } ?> <!-- end not single -->
+		</div><!-- .page -->
 
 		</div><!-- .padder -->
 	</div><!-- #content -->
-
 <?php get_footer(); ?>
--- wp-touch.php
+++ wp-touch.php
@@ -11,6 +11,13 @@ License: GPL2
 
 define( 'MY_PLUGIN_TEMPLATES', dirname( __FILE__ ) . '/templates' );
 
+//capabilities
+$role = get_role( 'administrator' );
+$role->add_cap( 'add_tasks' );
+$role->add_cap( 'manage_bugs' );
+$role->add_cap( 'manage_tasks' );
+$role->add_cap( 'manage_projects' );
+
 //create post type for projects
 add_action( 'init', 'track_create_post_type' );
 function track_create_post_type() {
@@ -18,11 +25,17 @@ function track_create_post_type() {
 		array(
 			'labels' => array(
 				'name' => __( 'Projects' ),
-				'singular_name' => __( 'Project' )
+				'singular_name' => __( 'Project' ),
+				'add_new_item' => 'Add Project',
+				'edit_item' => 'Edit Project',
+				'new_item' => 'New Project',
+				'view_item' => 'View Project',
+				'search_items' => 'Search Projects'
 			),
 		'public' => true,
 		'has_archive' => true,
-		'rewrite' => array('slug' => 'projects')
+		'rewrite' => array('slug' => 'projects'),
+		'supports' => array('title','editor','author' )
 		)
 	);
 register_post_type( 'track-tasks',
@@ -33,7 +46,8 @@ register_post_type( 'track-tasks',
 			),
 		'public' => true,
 		'has_archive' => true,
-		'rewrite' => array('slug' => 'tasks')
+		'rewrite' => array('slug' => 'tasks'),
+		'supports' => array('title','editor','comments')
 		)
 	);
 register_post_type( 'track-bugs',
@@ -44,28 +58,152 @@ register_post_type( 'track-bugs',
 			),
 		'public' => true,
 		'has_archive' => true,
-		'rewrite' => array('slug' => 'bugs')
+		'rewrite' => array('slug' => 'bugs'),
+		'supports' => array('title','editor','comments')
 		)
 	);
 }
+	
+	//taxonomies
+    add_action('init', 'build_taxonomies', 0);
+	function build_taxonomies() {
+	
+	//projects for tasks & bugs
+	register_taxonomy( 'projects', array('track-tasks','track-bugs'), array( 'hierarchical' => true, 'label' => 'Projects', 'query_var' => true, 'rewrite' => true ) );
+	register_taxonomy( 'status', array('track-tasks','track-bugs'), array( 'hierarchical' => true, 'label' => 'Status', 'query_var' => true, 'rewrite' => true ) );
+	register_taxonomy( 'milestone', array('track-tasks','track-bugs'), array( 'hierarchical' => true, 'label' => 'Milestone', 'query_var' => true, 'rewrite' => true ) );
+	register_taxonomy( 'priority', array('track-tasks','track-bugs'), array( 'hierarchical' => true, 'label' => 'Priority', 'query_var' => true, 'rewrite' => true ) );
+	}
 
+
+add_action('save_post', 'build_taxonomy');
+function build_taxonomy( $post_id ){
+
+    if ( defined('DOING_AUTOSAVE') && DOING_AUTOSAVE ) 
+        return $post_id;
+
+    if ( 'track-projects' == $_POST['post_type'] ){
+        if (!wp_is_post_revision($post_id)){
+            if (!term_exists( $_POST["post_title"], 'projects' )){
+                $termid = wp_insert_term( $_POST["post_title"], 'projects' );
+            }
+        }
+    }
+}	
+	
 add_filter('template_include', 'track_templates');
-
 function track_templates($template) {
 global $post;
 
 if($post->post_type == 'track-projects') {
 $template = MY_PLUGIN_TEMPLATES . '/projects.php';
 }
+if($post->post_type == 'track-bugs') {
+$template = MY_PLUGIN_TEMPLATES . '/bugs.php';
+}
+if($post->post_type == 'track-tasks') {
+//$template = MY_PLUGIN_TEMPLATES . '/tasks.php';
+}
 return $template;
 }
 
-function list_proeject_tasks() {
+function list_project_tasks($project_title) {
 
+echo "<h4>Tasks</h4>";
+add_task_button(); ?>
+<table class="wp-list-table widefat fixed posts" cellspacing="0">
+			<th>Name</th><th>Author</th><th>Priority</th><th>Status</th><th colspan="2">Milestone</th>
+			<?php
+
+  query_posts( array( 'post_type' => 'track-tasks', 'projects' => $project_title ) );
+  if ( have_posts() ) : while ( have_posts() ) : the_post();
+?>
+			
+
+
+	<tr>
+	<td><?php the_title(); ?></td>
+	<td><?php the_author(); ?></td>
+	<td><?php echo strip_tags( get_the_term_list( get_the_id(), 'priority', '', ', ', '' ) ); ?>
+	</td>
+	<td><?php echo strip_tags( get_the_term_list( get_the_id(), 'status', '', ', ', '' ) ); ?>
+	</td>
+	<td><?php echo strip_tags( get_the_term_list( get_the_id(), 'milestone', '', ', ', '' ) ); ?>
+	</td>
+	<td><?php task_actions(get_the_id(), get_permalink()); ?></td>
+	</tr>
+
+			
+<?php endwhile; endif; wp_reset_query(); ?>
+</table>
+<?php
 }
 
-function list_project_bugs() {
+function list_project_bugs($project_title) {
 
+echo "<h4>Bugs</h4>";
+add_bug_button(); ?>
+<table class="wp-list-table widefat fixed posts" cellspacing="0">
+			<th>Name</th><th>Author</th><th>Priority</th><th>Status</th><th colspan="2">Milestone</th> 
+			<?php
+
+  query_posts( array( 'post_type' => 'track-bugs', 'projects' => $project_title ) );
+  if ( have_posts() ) : while ( have_posts() ) : the_post();
+?>
+<tr>
+	<td><?php the_title(); ?></td>
+	<td><?php the_author(); ?></td>
+	<td><?php echo strip_tags( get_the_term_list( get_the_id(), 'priority', '', ', ', '' ) ); ?>
+	</td>
+	<td><?php echo $terms_as_text = strip_tags( get_the_term_list( get_the_id(), 'status', '', ', ', '' ) ); ?>
+	</td>
+	<td><?php echo $terms_as_text = strip_tags( get_the_term_list( get_the_id(), 'milestone', '', ', ', '' ) ); ?>
+	</td>
+	<td><?php bug_actions(get_the_id(), get_permalink()); ?></td>
+	</tr>
+
+			
+<?php endwhile; endif; wp_reset_query(); ?>
+</table>
+<?php
+}
+
+function add_task_button() {
+if( current_user_can('add_tasks') ) {
+echo "<a href='/wp-admin/post-new.php?post_type=track-tasks'>Add Task</a>";
+}
+}
+
+function add_bug_button() {
+if( current_user_can('add_tasks') ) {
+echo "<a href='javascript:addbug()'>Add Bug</a>";
+}
+}
+
+function task_actions($taskid, $link) {
+if( current_user_can('manage_tasks') ) {
+echo "
+<a target='_blank' href='/wp-admin/post.php?post=$taskid&action=edit'>Edit</a>
+<a href='$link'>View</a>
+";
+}
+}
+function bug_actions($bugid, $link) {
+if( current_user_can('manage_bugs') ) {
+echo "
+<a target='_blank' href='/wp-admin/post.php?post=$bugid&action=edit'>Edit</a>
+<a href='$link'>View</a>
+";
+}
+}
+
+function count_posts($type, $project_title) {
+$args = array(
+    'post_type' => $type,
+    'post_status' => 'published',
+	'projects' => $project_title
+);
+echo count( get_posts( $args ) );
 }
 
 ?>
