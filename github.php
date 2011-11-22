540942525894acd4930421ba0c7309cca49b546a
Louis Northmore <louis.northmore@gmail.com>
2011-11-22 12:37:36 +0000

    added github.php for auto sync!

---
+++ github.php
@@ -0,0 +1,9 @@
+<?php `git pull`;  
+
+/* 
+
+using github service hooks this auto updates the files. Neat huh?
+
+http://net.tutsplus.com/tutorials/other/the-perfect-workflow-with-git-github-and-ssh/
+
+*/
