c2a99512225e0526d82ebf5d4ddbeb44c5db5aa6
Louis Northmore <louis.northmore@gmail.com>
2011-11-22 12:53:28 +0000

    updates

--- github.php
+++ github.php
@@ -6,4 +6,6 @@ using github service hooks this auto updates the files. Neat huh?
 
 http://net.tutsplus.com/tutorials/other/the-perfect-workflow-with-git-github-and-ssh/
 
+Note: This had to be chmodded 644 on my webhost. Make sure you check that!
+
 */
